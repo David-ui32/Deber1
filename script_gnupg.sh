f64be9369b5076b150b2c8bc1fdb34efb45a8923
David-ui32 <cristo123456xx@gmail.com>
2026-03-26 23:33:00 +0000

    wich gpg

---
+++ script_gnupg.sh
@@ -0,0 +1,3 @@
+which gpg /  gpg --version #Describe the version of Gnu I have installed.
+gpg --full-generate-key #Generates a new key and requests shipment confirmation
+ gpg --list-secret-keys #List the keys I have, showing the data I have
